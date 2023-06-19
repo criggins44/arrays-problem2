f0a5eab1a2dbb58eab7b35253361326b42810685
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-19 19:27:33 -0400

    added comments to explain logic

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ let dogs = ['Poodle', 'Pitbull', 'Bulldog', 'Yorkie', 'Beagle']
 
 myFunction = array => {
 	let punctuation = ', ';
+//created a variable to be added to the objects in the array
+
 	return (array.join("" + punctuation));
+//used .join method convert array into a string and add  a comma to each element
+
 }
+
 console.log(myFunction(myArray));
 
 
